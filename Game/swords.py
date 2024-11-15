d63ed540856041cdde9a38c08cb333f2684e53f2
SeanditoBandito <rageradio+git@protonmail.com>
2024-11-15 15:11:44 -0500

    Deleted unnessescary files and updated the game

--- Game/swords.py
+++ Game/swords.py
@@ -6,21 +6,26 @@ import tomes
 swords = {
     "Sword of Eternity": {
         "Owner": "The Great Wizard",
+        "Tome": "Tome of Eternity",
     },
     "Sword of Darkness": {
         "Owner": "The Dark Lord",
+        "Tome": "Tome of Darkness",
 
      },
     "Sword of Dread": {
         "Owner": "The Great Wizard",
+        "Tome": "Tome of Dread",
 
     },
     "Sword of Pain": {
         "Owner": "The Dark Lord",
+        "Tome": "Tome of Pain",
 
     },
     "Sword of Pleasure": {
         "Owner": "The Great Wizard",
+        "Tome": "Tome of Pleasure",
 
     }
 }
--- Game/thegame.py
+++ Game/thegame.py
@@ -1,8 +1,9 @@
 #Imports
 import sys
-import time
+from time import sleep
 import random
 import swords
+import tomes
 from pprint import pprint
 
 # Create a function to roll a d20 and set it as tha variable resultd20
@@ -19,3 +20,10 @@ quest_sword = random.choice(list(swords.swords.keys()))
 
 # Print the owner value of the variable quest_sword
 print("You have been tasked with finding the " + quest_sword + " owned by " + swords.swords[quest_sword]["Owner"] + ".")
+sleep(2)
+print("You set out on your journey to find the " + quest_sword + ".")
+sleep(2)
+print("You come across a tome that may hold the secret to finding the " + quest_sword + ".")
+sleep(2)
+print("Do you want to pick up the tome?")
+tome_decision = input("Yes or No: ")
